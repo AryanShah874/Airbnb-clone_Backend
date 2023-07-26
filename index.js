f4f21cb1e4859a8fc41fa0b73e1dab3ab1b9204b
Aryan Shah <aryanwork10@gmail.com>
2023-07-27 02:41:21 +0530

    fourth commit

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post("/login", async function(req, res){
                                     console.log(err);
                                 }
                                 else{
-                                    res.cookie("token", token, {domain: "https://airbnb-clone-backend-one.vercel.app", httpOnly: true, sameSite: "lax"}).json({"user": foundUser, "success": "Login Successful."});
+                                    res.cookie("token", token, {domain: ".vercel.app", httpOnly: true, sameSite: "lax"}).json({"user": foundUser, "success": "Login Successful."});
                                 }
                             });
                         }
